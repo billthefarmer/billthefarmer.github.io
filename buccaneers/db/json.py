d65ec0f7c2e9d3af54ebde081bd27319df9dadb4
Bill Farmer <williamjfarmer@yahoo.co.uk>
2015-07-24 10:35:17 +0100

    Original site restored

--- buccaneers/db/json.py
+++ buccaneers/db/json.py
@@ -5,8 +5,13 @@ import sqlite3 as db
 
 conn = db.connect('buccaneers.db')
 c = conn.cursor()
+e = []
 
-for row in c.execute('SELECT * FROM events'):
-    print 'row: ', row
+for row in c.execute('SELECT * FROM events ORDER BY date'):
+    n = row[0]
+    d = row[1]
+    t = row[2]
+    print 'row: ', n, d, t
+    print row
 
 conn.close()
