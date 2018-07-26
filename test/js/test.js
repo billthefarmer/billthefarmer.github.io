7667b7b822d12cf00e2c5b18946cf930ad7f03b7
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-07-26 16:23:11 +0100

    Update test

--- test/js/test.js
+++ test/js/test.js
@@ -10,8 +10,7 @@
 jQuery(document).ready(function($) {
 
     let data =
-        {intro:
-         "Please answer the following questions as carefully and accurately as you can",
+        {intro: "Please answer the following questions as carefully and accurately as you can",
          questions:
          [{q: "I prefer dealing with",
            t: "B",
@@ -33,14 +32,75 @@ jQuery(document).ready(function($) {
           {q: "I look at",
            t: "F",
            a: ["The details", "The big picture"],
+           v: [2, 4]},
+          {q: "My process to make decisions is more",
+           t: "B",
+           a: ["Thinking", "Feeling"],
+           v: [2, 4]},
+          {q: "When with other people I like",
+           t: "C",
+           a: ["Listening to ideas", "Expressing ideas"],
+           v: [2, 4]},
+          {q: "I tend to choose",
+           t: "D",
+           a: ["Discovering new approaches",
+               "Taking what has worked for others"],
+           v: [2, 4]},
+          {q: "My modus operandi is",
+           t: "E",
+           a: ["To take charge", "To build a solution"],
+           v: [2, 4]},
+          {q: "I'd describe myself as a",
+           t: "F",
+           a: ["Single tasker", "Multitasker"],
+           v: [2, 4]},
+          {q: "I prefer to work with",
+           t: "B",
+           a: ["Facts", "Intuition"],
+           v: [2, 4]},
+          {q: "Social interaction",
+           t: "C",
+           a: ["Depletes my energy", "Gives me energy"],
+           v: [2, 4]},
+          {q: "I want",
+           t: "D",
+           a: ["Tested processes", "Flexible options"],
+           v: [2, 4]},
+          {q: "When problems need to be solved I gravitate towards",
+           t: "E",
+           a: ["Getting work done to solve the problem",
+               "Advising on how the problem can be solved"],
+           v: [2, 4]},
+          {q: "I prefer",
+           t: "F",
+           a: ["Having one goal", "Having lots of projects to work on"],
            v: [2, 4]}],
-         results:
-         {A: 0,
-          B: 0,
-          C: 0,
-          D: 0,
-          E: 0,
-          F: 0}};
+         last:
+         {q: "Where are you on your business journey right now?",
+          a: ["Deciding on my business idea/niche",
+              "I know what I want to do, I haven't started",
+              "Started, working towards my first clients",
+              "Got my first clients, looking to grow"],
+          t: "J",
+          v: [2, 4, 6, 8]},
+        matrix:
+         {A: [["Secret Weapon", "Dynamic Pioneer"],
+              ["Creative Powerhouse", "Visionary Coach"]],
+          B: ["Analytical", "Strategic", "Emotive", "Intuitive"],
+          C: ["Reflector", "Relator", "Engager", "Presenter"],
+          D: ["Innovative", "Agile", "Pragmatic", "Trusted"],
+          E: ["Producer", "Developer", "Architect", "Torch-bearer"],
+          F: ["Singular", "Related", "Varied", "Diverse"]
+         },
+        };
+
+    // let data = {};
+
+    // $.getJSON("js/test.json", function(json) {
+    //     data = json;
+    // });
+
+    $("#data").html(JSON.stringify(data, null, 2));
 
     // Set up buttons
     $("input[type=button]").button();
@@ -50,12 +110,18 @@ jQuery(document).ready(function($) {
 
     $("#intro").html(data.intro);
 
-    let question;
+    let question = 0;
+    let value = 0;
+    
     let questions = data.questions;
+    let last = data.last;
+    let results = {A: [0, 0], B: 0, C: 0, D: 0, E: 0, F: 0, J: 0};
+    let matrix = data.matrix;
 
     // Process the start button
     $("#start").click(function() {
         question = 0;
+        $("#data").css("display", "none");
         $(".intro").fadeOut(function(){$(".question").fadeIn()});
         $("#question").html(questions[question].q);
         $("#label-1").html(questions[question].a[0]);
@@ -65,35 +131,117 @@ jQuery(document).ready(function($) {
         $("input[type=radio]").prop("checked", false);
     });
 
-    // Process the radio buttons
-    $("input[type=radio]").click(function() {
+    // Process the back button
+    $("#back").click(function() {
+        question--;
         let type = questions[question].t;
-        data.results[type] += +$(this).attr("value");
+        results[type] -= value;
+        if (question == 0)
+            $("input.back").css("display", "none");
+        $(".question").fadeOut(function() {
+            $("#question").html(questions[question].q);
+            $("#label-1").html(questions[question].a[0]);
+            $("#radio-1").attr("value", questions[question].v[0]);
+            $("#label-2").html(questions[question].a[1]);
+            $("#radio-2").attr("value", questions[question].v[1]);
+            $("input[type=radio]").prop("checked", false);
+            $(".question").fadeIn();
+        });
+    });
+
+    // Process the prev button
+    $("#prev").click(function() {
+        question--;
+        let type = questions[question].t;
+        results[type] -= value;
+        $("div.last").fadeOut(function() {
+            $("#question").html(questions[question].q);
+            $("#label-1").html(questions[question].a[0]);
+            $("#radio-1").attr("value", questions[question].v[0]);
+            $("#label-2").html(questions[question].a[1]);
+            $("#radio-2").attr("value", questions[question].v[1]);
+            $("input[type=radio]").prop("checked", false);
+            $(".question").fadeIn();
+        });
+    });
+
+    // Process the radio buttons
+    $("input[type=radio].answer").click(function() {
+        let type = questions[question].t;
+        value = +$(this).attr("value");
+        results[type] += value;
         question++;
         if (question < questions.length)
         {
             let type = questions[question].t;
-            $(".question").fadeOut(function() {
+            $("div.question").fadeOut(function() {
                 $("#question").html(questions[question].q);
                 $("#label-1").html(questions[question].a[0]);
                 $("#radio-1").attr("value", questions[question].v[0]);
                 $("#label-2").html(questions[question].a[1]);
                 $("#radio-2").attr("value", questions[question].v[1]);
                 $("input[type=radio]").prop("checked", false);
-                $(".question").fadeIn()});
+                $("input.back").css("display", "block");
+                $("div.question").fadeIn();
+            });
         }
 
         else
         {
-            $(".question").fadeOut(function(){$(".result").fadeIn()});
-            $("#result").html(JSON.stringify(data.results, null, 4));
-            console.log(data.results);
+            $("div.question").fadeOut(function() {
+                $("#last").html(last.q);
+                $("#label-3").html(last.a[0]);
+                $("#radio-3").attr("value", last.v[0]);
+                $("#label-4").html(last.a[1]);
+                $("#radio-4").attr("value", last.v[1]);
+                $("#label-5").html(last.a[2]);
+                $("#radio-5").attr("value", last.v[2]);
+                $("#label-6").html(last.a[3]);
+                $("#radio-6").attr("value", last.v[3]);
+                $("input[type=radio]").prop("checked", false);
+                $("div.last").fadeIn();
+            });
         }
     });
 
+    //Process the last radio buttons
+    $("input[type=radio].last").click(function() {
+        let type = last.t;
+        value = +$(this).attr("value");
+        results[type] += value;
+        $("div.last").fadeOut(function() {
+            results.A[0] = results.B;
+            results.A[1] = results.E;
+            $("#result").html(JSON.stringify(results, null, 4));
+            $("#calc").html(JSON.stringify(calculate(results, matrix),
+                                          null, 4));
+            $("div.result").fadeIn();
+            console.log(results);
+            console.log(calculate(results, matrix));
+        });
+    });
+
+    // Process the again button
     $("#again").click(function() {
         $(".result").fadeOut(function(){$(".intro").fadeIn()});
-        for (let [key, value] in data.results)
-            data.results[key] = 0;
+        for (let [key, value] in results)
+            results[key] = 0;
     });
 });
+
+function calculate(results, matrix) {
+    let b = (results.B / 2) - 3;
+    let c = (results.C / 2) - 3;
+    let d = (results.D / 2) - 3;
+    let e = (results.E / 2) - 3;
+    let f = (results.F / 2) - 3;
+
+    let result = {A: matrix.A[Math.trunc(b / 2)][Math.trunc(e / 2)],
+                  B: matrix.B[b],
+                  C: matrix.C[c],
+                  D: matrix.D[d],
+                  E: matrix.E[e],
+                  F: matrix.F[f]};
+
+    return result;
+}
