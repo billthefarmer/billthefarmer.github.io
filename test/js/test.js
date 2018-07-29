90c585898942bcd9384efc4d15c25e0325627a55
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-07-29 19:02:02 +0100

    Update quiz

--- test/js/test.js
+++ test/js/test.js
@@ -8,92 +8,7 @@
 // Copyright (C) 2018 Bill Farmer
 
 jQuery(document).ready(function($) {
-    /*
-    let data =
-        {intro: "Please answer the following questions as carefully and accurately as you can",
-         questions:
-         [{q: "I prefer dealing with",
-           t: "B",
-           a: ["Numbers", "Images"],
-           v: [2, 4]},
-          {q: "I have more fun",
-           t: "C",
-           a: ["Talking one on one with one person",
-               "Meeting lots of people"],
-           v: [2, 4]},
-          {q: "I like options to be",
-           t: "D",
-           a: ["Cutting edge", "Proven"],
-           v: [2, 4]},
-          {q: "I prefer to",
-           t: "E",
-           a: ["Be guided", "Guide"],
-           v: [2, 4]},
-          {q: "I look at",
-           t: "F",
-           a: ["The details", "The big picture"],
-           v: [2, 4]},
-          {q: "My process to make decisions is more",
-           t: "B",
-           a: ["Thinking", "Feeling"],
-           v: [2, 4]},
-          {q: "When with other people I like",
-           t: "C",
-           a: ["Listening to ideas", "Expressing ideas"],
-           v: [2, 4]},
-          {q: "I tend to choose",
-           t: "D",
-           a: ["Discovering new approaches",
-               "Taking what has worked for others"],
-           v: [2, 4]},
-          {q: "My modus operandi is",
-           t: "E",
-           a: ["To take charge", "To build a solution"],
-           v: [2, 4]},
-          {q: "I'd describe myself as a",
-           t: "F",
-           a: ["Single tasker", "Multitasker"],
-           v: [2, 4]},
-          {q: "I prefer to work with",
-           t: "B",
-           a: ["Facts", "Intuition"],
-           v: [2, 4]},
-          {q: "Social interaction",
-           t: "C",
-           a: ["Depletes my energy", "Gives me energy"],
-           v: [2, 4]},
-          {q: "I want",
-           t: "D",
-           a: ["Tested processes", "Flexible options"],
-           v: [2, 4]},
-          {q: "When problems need to be solved I gravitate towards",
-           t: "E",
-           a: ["Getting work done to solve the problem",
-               "Advising on how the problem can be solved"],
-           v: [2, 4]},
-          {q: "I prefer",
-           t: "F",
-           a: ["Having one goal", "Having lots of projects to work on"],
-           v: [2, 4]}],
-         last:
-         {q: "Where are you on your business journey right now?",
-          a: ["Deciding on my business idea/niche",
-              "I know what I want to do, I haven't started",
-              "Started, working towards my first clients",
-              "Got my first clients, looking to grow"],
-          t: "J",
-          v: [2, 4, 6, 8]},
-        matrix:
-         {A: [["Secret Weapon", "Dynamic Pioneer"],
-              ["Creative Powerhouse", "Visionary Coach"]],
-          B: ["Analytical", "Strategic", "Emotive", "Intuitive"],
-          C: ["Reflector", "Relator", "Engager", "Presenter"],
-          D: ["Innovative", "Agile", "Pragmatic", "Trusted"],
-          E: ["Producer", "Developer", "Architect", "Torch-bearer"],
-          F: ["Singular", "Related", "Varied", "Diverse"]
-         },
-        };
-    */
+
     // let data = {};
 
     // $.getJSON("js/test.json", function(json) {
@@ -113,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#max").progressbar({value: 100});
 
     // Set up radio buttons
-    // $("input[type=radio]").checkboxradio();
+    $("input[type=radio]").checkboxradio();
 
     $("#intro").html(data.intro);
 
@@ -148,6 +63,7 @@ jQuery(document).ready(function($) {
             $("#radio-1").attr("value", questions[question].v[1]);
         }
         $("input[type=radio]").prop("checked", false);
+        $("input[type=radio]").checkboxradio("refresh");
     });
 
     // Process the back button
@@ -177,6 +93,7 @@ jQuery(document).ready(function($) {
                 $("#radio-1").attr("value", questions[question].v[1]);
             }
             $("input[type=radio]").prop("checked", false);
+            $("input[type=radio]").checkboxradio("refresh");
             $(".question").fadeIn();
         });
     });
@@ -206,6 +123,7 @@ jQuery(document).ready(function($) {
                 $("#radio-1").attr("value", questions[question].v[1]);
             }
             $("input[type=radio]").prop("checked", false);
+            $("input[type=radio]").checkboxradio("refresh");
             $(".question").fadeIn();
         });
     });
@@ -239,6 +157,7 @@ jQuery(document).ready(function($) {
                     $("#radio-1").attr("value", questions[question].v[1]);
                 }
                 $("input[type=radio]").prop("checked", false);
+                $("input[type=radio]").checkboxradio("refresh");
                 $("input.back").css("display", "block");
                 $("div.question").fadeIn();
             });
@@ -256,6 +175,7 @@ jQuery(document).ready(function($) {
                 $("#radio-5").attr("value", last.v[2]);
                 $("#label-6").html(last.a[3]);
                 $("#radio-6").attr("value", last.v[3]);
+                $("input[type=radio]").checkboxradio("refresh");
                 $("input[type=radio]").prop("checked", false);
                 $("div.last").fadeIn();
             });
